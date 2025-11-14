a3bc05f72cb16426eb514df8a6e024457e246fcf
Abdifatah <mursalabdifatah17@gmail.com>
2025-11-14 17:42:24 +0300

    python-p3-anagram-detector-lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        matches = []
+
+        for w in word_list:
+            lw = w.lower()
+            if lw != self.word and sorted(lw) == sorted_word:
+                matches.append(w)
+
+        return matches
--- lib/debug.py
+++ lib/debug.py
@@ -3,4 +3,11 @@
 from anagram import Anagram
 
 if __name__ == '__main__':
+    detector = Anagram("listen")
+    words = ["enlists", "google", "inlets", "banana"]
+
+    # This lets you test the match() method inside ipdb
     import ipdb; ipdb.set_trace()
+
+    # Example call (run this inside ipdb):
+    # detector.match(words)
